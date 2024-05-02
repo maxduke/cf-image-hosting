043ac61e43dd1c0e4e658687e6505f403a8f8b55
maxduke <maxduke@gmail.com>
2024-05-02 15:56:11 +0800

    adjustment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
+import { basicAuth } from 'hono/basic-auth'
 import { cors } from "hono/cors";
 import { Home } from "./Home";
 
-const app = new Hono<{ Bindings: { API_HOST: string } }>();
+const app = new Hono<{ Bindings: { API_HOST: string, USERNAME: string, PASSWORD: string } }>();
+
+app.use('/', async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
+  })
+  return auth(c, next)
+});
 
 app.get("/", (c) => c.html(<Home />));
 
@@ -21,10 +30,18 @@ app.post("/upload", cors(), async (c) => {
 });
 
 app.get("/file/:name", async (c) => {
-  const response = await fetch(`${c.env.API_HOST}/file/${c.req.param("name")}`);
-  return c.newResponse(response.body as ReadableStream, {
-    headers: response.headers,
-  });
+  const referrer = c.req.header('Referer') || '';
+  const url = referrer.split('/')[2] || '';
+  const min_ulr = url.split(':')[0] || '';
+  if(referrer && c.env.whiteList.includes(min_ulr) || !referrer) {
+    // 访问域名在白名单内，放行 !referer表示直接访问图片(比如浏览器地址栏输入图片地址)
+    const response = await fetch(`${c.env.API_HOST}/file/${c.req.param("name")}`);
+    return c.newResponse(response.body as ReadableStream, {
+      headers: response.headers,
+    });
+  } else {
+    return fetch('https://pic.diydoutu.com/bq/1061.jpg');
+  }
 });
 
 export default app;
